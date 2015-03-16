92f0cc5588abf30e07b17d31f4a0378d2b711f84
gandarez <gandarez@gmail.com>
2015-03-16 17:10:04 -0300

    Added Top10 plugin

---
+++ src/Roadkill.Plugins.Top10/Top10.cs
@@ -0,0 +1,80 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using Roadkill.Core.Database;
+using Roadkill.Core.Mvc.ViewModels;
+using Roadkill.Core.Plugins;
+
+#endregion
+
+namespace Roadkill.Plugins.Top10
+{
+	public class Top10 : TextPlugin
+	{
+		private IRepository _repository;
+		private static Regex _regex = new Regex(@"(?<!{)(?:\{Top10\})(?!})", RegexOptions.Compiled);
+
+		public override string Id
+		{
+			get { return "Top10"; }
+		}
+
+		public override string Name
+		{
+			get { return "Top Ten Pages"; }
+		}
+
+		public override string Description
+		{
+			get { return "Displays the top new/modified pages."; }
+		}
+
+		public override string Version
+		{
+			get { return "1.0.0"; }
+		}
+
+		public Top10(IRepository repository) : base()
+		{
+			_repository = repository;
+		}
+
+        public override void OnInitializeSettings(Settings settings)
+        {
+            settings.SetValue("Quantity", "10", SettingFormType.Textbox);
+        }
+
+		public override string AfterParse(string html)
+		{
+            var match = _regex.Match(html);
+            if (!match.Success) return html;
+
+            int qty = 10;
+            int.TryParse(Settings.GetValue("Quantity"), out qty);
+
+			List<Page> topPages = _repository.AllPages().OrderByDescending(p => p.ModifiedOn).Take(10).ToList();
+			if (topPages.Count == 0) return _regex.Replace(html, string.Empty);
+
+			UrlHelper helper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendLine("<ol class=\"clear\">");
+			foreach (Page page in topPages) {
+				sb.AppendFormat("<li><a href=\"{0}\">{1}</a> <span class=\"smaller\">{2} {3}</span></li>", new object[] {
+					helper.Action("Index", "Wiki", new {
+						id = page.Id,
+						title = PageViewModel.EncodePageTitle(page.Title)
+					}),
+					page.Title, page.ModifiedOn.ToLongDateString(), page.ModifiedOn.ToShortTimeString()
+				});
+			}
+			sb.AppendLine("</ol>");
+			return _regex.Replace(html, sb.ToString());
+		}
+	}
+}
